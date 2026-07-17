ccfb4a503e834ec82478f84324d290caa2d5a1cb
EX-P-00795\EX-P-00795 <ayuki-takahashi@e-xtreme.co.jp>
2026-07-17 11:53:08 +0900

    修正して整理して

--- App.java
+++ App.java
@@ -1,15 +1,15 @@
-public static void main(String[] arg){
-    int[] a = {0,1,2,3,4};
-    for(int b :a){
-    int b = switch (a){
-    case 0  -> {
-        yield 5;
+public class Main {
+public static void main(String[] args) {
+    int[] a = {0, 1, 2, 3, 4};
+    for (int b : a) {
+        int result = switch (b) {
+            case 0 -> 5;
+            case 1 -> 7;
+            case 2 -> 4;
+            case 8 -> 19;
+            default -> 6;
+        };
+    System.out.println(result);
     }
-    case -> 7
-    case 2 -> 4
-    default -> 6 
-    };
-System.out.printline(b);
     }
 }
-
